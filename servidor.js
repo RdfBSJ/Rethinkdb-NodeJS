71837558496c878c33d2222471f4f397642eacfd
Rodolfo Bartolo <rolphbsj@gmail.com>
2017-02-02 09:11:16 -0600

    Refactor a lineas de codigo
    Se eliminó el archivo app.js, se agrego la clase container al
    layout.jade y se agrego un título a la vista lista.jade

--- servidor.js
+++ servidor.js
@@ -5,13 +5,12 @@ var method_override = require('method-override');
 var app_password = "admin";
 var connection = null;
 
-r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
+/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
     if (err) throw err;
     connection = conn;
-})
+})*/
 
 var app = express();
-app.listen(8081);
 
 app.set("view engine", "jade");
 app.use(express.static("public"));
@@ -19,12 +18,76 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(method_override("_method"));
 
+app.listen(8081);
+
 app.get("/", function(solicitud, respuesta) {
     respuesta.render("index");
 });
 
+app.get("/productos/nuevo", function(solicitud, respuesta) {
+    respuesta.render("productos/nuevo");
+});
+
+app.post("/producto", function(solicitud, respuesta) {
+    //console.log(solicitud.body);
+
+    r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
+        if (err) throw err;
+        connection = conn;
+
+        r.table('Productos').insert({
+            Nombre: solicitud.body.nombre,
+            Descripcion: solicitud.body.descripcion,
+            Precio: solicitud.body.precio,
+            Clave: solicitud.body.clave
+        }).run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        })
+    })
+
+    respuesta.render("productos/nuevo");
+});
+
+app.get("/productos/lista", function(solicitud, respuesta) {
+    r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
+        if (err) throw err;
+        connection = conn;
+
+        r.table('Productos').run(connection, function(err, cursor) {
+            if (err) throw err;
+            cursor.toArray(function(err, result) {
+                if (err) throw err;
+                console.log(JSON.stringify(result, null, 2));
+                respuesta.render("productos/lista", { productos: result })
+            });
+        });
+    })
+});
+
 app.get("/admin/form", function(solicitud, respuesta) {
     respuesta.render("admin/form");
+});
+
+app.post("/admin", function(solicitud, respuesta) {
+    //console.log(solicitud.body);
+    if (solicitud.body.clave == app_password) {
+        r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
+            if (err) throw err;
+            connection = conn;
+
+            r.table('Productos').run(connection, function(err, cursor) {
+                if (err) throw err;
+                cursor.toArray(function(err, result) {
+                    if (err) throw err;
+                    console.log(JSON.stringify(result, null, 2));
+                    respuesta.render("admin/index", { productos: result })
+                });
+            });
+        })
+    } else {
+        respuesta.redirect("/");
+    }
 });
 
 app.get("/productos/editar/:id", function(solicitud, respuesta) {
@@ -118,150 +181,3 @@ app.delete("/borrar/:id", function(solicitud, respuesta) {
     }
 })
 
-app.get("/productos/lista", function(solicitud, respuesta) {
-    r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-        if (err) throw err;
-        connection = conn;
-
-        r.table('Productos').run(connection, function(err, cursor) {
-            if (err) throw err;
-            cursor.toArray(function(err, result) {
-                if (err) throw err;
-                console.log(JSON.stringify(result, null, 2));
-                respuesta.render("productos/lista", { productos: result })
-            });
-        });
-    })
-});
-
-app.get("/productos/nuevo", function(solicitud, respuesta) {
-    respuesta.render("productos/nuevo");
-});
-
-app.post("/producto", function(solicitud, respuesta) {
-    //console.log(solicitud.body);
-
-    r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-        if (err) throw err;
-        connection = conn;
-
-        r.table('Productos').insert({
-            Nombre: solicitud.body.nombre,
-            Descripcion: solicitud.body.descripcion,
-            Precio: solicitud.body.precio,
-            Clave: solicitud.body.clave
-        }).run(connection, function(err, result) {
-            if (err) throw err;
-            console.log(JSON.stringify(result, null, 2));
-        })
-    })
-
-    respuesta.render("productos/nuevo");
-});
-
-app.post("/admin", function(solicitud, respuesta) {
-    //console.log(solicitud.body);
-    if (solicitud.body.clave == app_password) {
-        r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-            if (err) throw err;
-            connection = conn;
-
-            r.table('Productos').run(connection, function(err, cursor) {
-                if (err) throw err;
-                cursor.toArray(function(err, result) {
-                    if (err) throw err;
-                    console.log(JSON.stringify(result, null, 2));
-                    respuesta.render("admin/index", { productos: result })
-                });
-            });
-        })
-    } else {
-        respuesta.redirect("/");
-    }
-});
-
-
-/*r.connect({host: "localhost", port: "28015"}, function(err, conn){
-    if(err) throw err;
-    connection = conn;
-
-    r.db('test').tableCreate('categorias').run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    })
-})*/
-
-/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    r.table('categorias').insert([{
-        nombre: "Mangueras",
-        cantidad: "600",
-        unidad: "metros"
-    }, {
-        nombre: "Pintura",
-        cantidad: "100",
-        unidad: "tambores"
-    }, {
-        nombre: "Guantes",
-        cantidad: "450",
-        unidad: "piezas"
-    }]).run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    })
-})*/
-
-/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    r.table('categorias').run(connection, function(err, cursor) {
-        if (err) throw err;
-        cursor.toArray(function(err, result) {
-            if (err) throw err;
-            console.log(JSON.stringify(result, null, 2));
-        });
-    });
-})*/
-
-/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    r.table('categorias').update({tipo: "zinc"}).
-    run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    });
-})*/
-
-/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    r.table('categorias').
-    filter(r.row("nombre").eq("Pintura")).
-    update({precio: "$500.00"}).
-    run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    });
-})*/
-
-/*r.connect({ host: "localhost", port: "28015" }, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-    //codigo pendiente por tener diferente estructura.
-    r.table('categorias').filter(r.row("nombre").eq("Tornillos")).
-    update({
-        tipos: r.row("posts").append({
-            title: "Shakespeare",
-            content: "What a piece of work is man..."
-        })
-    }).run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    });
-})*/
